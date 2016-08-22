512a265c58ee66ff622d09f2f054e274bc68bc96
yunsubaek <yunsubaek@naver.com>
2016-08-22 20:44:08 +0900

    Update alert.cs

--- alert.cs
+++ alert.cs
@@ -5,16 +5,12 @@ class Program
 {
     static void Main(String[] args)
     {
-        DateTime cmpTime = new DateTime(2016, 8, 22, 17, 00, 00);
-        int result = 1;
-        while (result != 0)
+        DateTime cmpTime = new DateTime(2016, 8, 22, 20, 59, 30);
+        while (true)
         {
-            DateTime curTime = DateTime.Now;
-            if (cmpTime == curTime) return;
-            result = DateTime.Compare(cmpTime, curTime);
-            if (result <= 0)
+            if (DateTime.Compare(cmpTime, DateTime.Now) <= 0)
             {
-                System.Diagnostics.Process.Start("C:\\Users\\yunsubaek_df\\Downloads\\resttime.txt");
+                System.Diagnostics.Process.Start("C:\\Users\\yunsubaek_df\\Downloads\\conference.txt");
                 break;
             }
             System.Threading.Thread.Sleep(1000);
